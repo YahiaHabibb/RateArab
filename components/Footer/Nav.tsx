0199b6a1f7c900724f288ea0b36013e7c94982a3
YahiaHabibb <yahiahabibtech@gmail.com>
2025-03-10 21:38:43 +0200

    Rate-Arab-Events1

---
+++ components/Footer/Nav.tsx
@@ -0,0 +1,14 @@
+
+const Nav = () => {
+    return (
+        <div>
+            <header>
+                <div className="bg-[url()]">
+
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Nav
